d562a42755b173ca9ee4f30d6aecc8fab14e31d0
chris315-psyga <chris.maruli7@gmail.com>
2026-02-18 12:56:44 +0700

    Tugas 1

---
+++ src/main/java/com/example/springmvclab/controller/AboutController.java
@@ -0,0 +1,21 @@
+package com.example.springmvclab.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class AboutController {
+    @GetMapping("/about")
+    public String about(Model model){
+        model.addAttribute("appName", "Spring MVC Lab");
+        model.addAttribute("version", "1.0");
+        model.addAttribute("author", "Chris Maruli Siagian");
+        model.addAttribute("technologies", List.of("Spring Boot", "Thymeleaf", "Bootstrap", "Java 25"));
+
+        return "about";
+    }
+}
